45c296839f050e2826ac313519265c74019e9823
Valgulious <dmitryfedorenkof@gmail.com>
2017-11-29 14:55:40 +1000

    BinarySearch

--- Search/Search/main.cpp
+++ Search/Search/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int BinarySearch (int*,int);
-void BubbleSort (int*);
+int trueSort(int*);
 
 int const SIZE_OF_ARRAY = 10;
 int array[SIZE_OF_ARRAY];
@@ -14,13 +14,14 @@ int main() {
     setlocale(LC_ALL, "rus");
     srand(time(NULL));
 
-    for (int i=0;i<SIZE_OF_ARRAY;i++) array[i] = rand()%100;
-    BubbleSort(array);
-    for (int i=0;i<SIZE_OF_ARRAY;i++) cout << array[i] << ' ';
+    for (int i=0;i<SIZE_OF_ARRAY;i++){
+        array[i] = i*i;
+        cout << array[i] << ' ';
+    }
     cout << endl << "Введите искомое число" << endl;
     cin >> key;
     result = BinarySearch(array,key);
-    if (result) cout << "Искомое число стоит под номером: " << result << endl;
+    if (result) cout << "Искомое число стоит на позиции: " << result << endl;
     else cout << "Поиск не дал результатов" << endl;
     return 0;
 }
@@ -28,18 +29,18 @@ int main() {
 int BinarySearch (int *array, int key) {
     int left(0), right(SIZE_OF_ARRAY), mid(0);
 
-    while (left <= right){
-        mid = (left+right)/2;
-        if (key == array[mid]) return mid+1;
-        else if (key < array[mid]) right = mid - 1;
-        else if (key > array[mid]) left = mid + 1;
-    }
+    if (trueSort(array)){
+        while (left <= right){
+            mid = (left+right)/2;
+            if (key == array[mid]) return mid+1;
+            else if (key < array[mid]) right = mid - 1;
+            else if (key > array[mid]) left = mid + 1;
+        }
+
+    } else return 0;
     return 0;
 }
 
-void BubbleSort (int *array){
-    for (int i=0;i<SIZE_OF_ARRAY-1;i++) {
-        for (int j = 0; j < SIZE_OF_ARRAY - i; j++)
-            if (array[j-1] > array[j]) swap(array[j-1], array[j]);
-    }
+int trueSort (int *array){
+    for (int i=1;i<SIZE_OF_ARRAY;i++) if (array[i-1]>array[i]) return 0;
 }

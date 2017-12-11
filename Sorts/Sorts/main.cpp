0ad2e6b45bf49e33e8f8f8bcdf479ff6f7ae8b6c
Valgulious <dmitryfedorenkof@gmail.com>
2017-12-11 22:02:59 +1000

    Sorts done

--- Sorts/Sorts/main.cpp
+++ Sorts/Sorts/main.cpp
@@ -3,20 +3,29 @@
 using namespace std;
 
 void NonRecursiveQuickSort (int[]);
+void TooWayInsertion (int[]);
 
 const int SIZE_OF_ARRAY = 10;
 
 int main() {
-    int a[SIZE_OF_ARRAY];
+    setlocale(LC_ALL, "rus");
+
+    int array_for_QuickSort[SIZE_OF_ARRAY], array_for_InsSort[SIZE_OF_ARRAY];
     srand(time(NULL));
 
     for (int i=0;i<SIZE_OF_ARRAY;i++) {
-        a[i] = 0;
-        cout << a[i] << ' ';
+        array_for_QuickSort[i] = -50 + rand()%100;
+        array_for_InsSort[i] = array_for_QuickSort[i];
+        cout << array_for_QuickSort[i] << ' ';
     }
+
+    cout << endl << "Результат Быстрой сортировки: ";
+    NonRecursiveQuickSort(array_for_QuickSort);
+    for (int i=0;i<SIZE_OF_ARRAY;i++) cout << array_for_QuickSort[i] << ' ';
+    cout << endl << "Результат сортировки Вставками: ";
+    TooWayInsertion(array_for_InsSort);
+    for (int i=0;i<SIZE_OF_ARRAY;i++) cout << array_for_InsSort[i] << ' ';
     cout << endl;
-    NonRecursiveQuickSort(a);
-    for (int i=0;i<SIZE_OF_ARRAY;i++) cout << a[i] << ' ';
     return 0;
 }
 
@@ -43,4 +52,55 @@ void NonRecursiveQuickSort (int array[]){
             R=j;
         } while (L < R);
     } while (s >= 0);
+}
+
+void TooWayInsertion (int array[]){
+    int array_for_sort[2*SIZE_OF_ARRAY+1];
+    int left(0), right(0), k((2*SIZE_OF_ARRAY+1)/2), j(0);
+
+    for (int i=0;i<2*SIZE_OF_ARRAY+1;i++) array_for_sort[i] = 0;
+
+    array_for_sort[k] = array[0];
+    for (int i=1;i<SIZE_OF_ARRAY;i++){
+        while ((k+right != k) && (array[i] > array_for_sort[k])){
+            k++; left++; right--;
+        }
+        while ((k+left != k) && (array[i] < array_for_sort[k])){
+            k--; left--; right++;
+        }
+        if (array[i] > array_for_sort[k]){
+
+            if (left < right) {
+                for (int j=k-left-1;j<k;j++){
+                    swap(array_for_sort[j],array_for_sort[j+1]);
+                }
+                array_for_sort[k] = array[i];
+                left++;
+            } else {
+                for (int j=k+right+1;j>k+1;j--){
+                    swap(array_for_sort[j],array_for_sort[j-1]);
+                }
+                array_for_sort[k+1] = array[i];
+                right++;
+            }
+        } else {
+            if (left < right) {
+                for (int j=k-left-1;j<k-1;j++){
+                    swap(array_for_sort[j],array_for_sort[j+1]);
+                }
+                array_for_sort[k-1] = array[i];
+                left++;
+            } else {
+                for (int j=k+right+1;j>k;j--){
+                    swap(array_for_sort[j],array_for_sort[j-1]);
+                }
+                array_for_sort[k] = array[i];
+                right++;
+            }
+        }
+    }
+    for (int i=k-left;i<SIZE_OF_ARRAY+(k-left);i++){
+        array[j] = array_for_sort[i];
+        j++;
+    }
 }

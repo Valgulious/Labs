c8debacdf2777ff614e8198494880259573d6b3a
Valgulious <dmitryfedorenkof@gmail.com>
2017-12-20 14:35:14 +1000

    Add some changes

--- Sorts/Sorts/main.cpp
+++ Sorts/Sorts/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
+/* ВНИМАНИЕ!!!! Сколько времнени занимает работа сортировки?*/
 void NonRecursiveQuickSort (int[]);
 void TooWayInsertion (int[]);
 
 const int SIZE_OF_ARRAY = 10;
+
+int swapCount(0), ifCount(0);
 
 int main() {
     setlocale(LC_ALL, "rus");
@@ -22,33 +24,53 @@ int main() {
     cout << endl << "Результат Быстрой сортировки: ";
     NonRecursiveQuickSort(array_for_QuickSort);
     for (int i=0;i<SIZE_OF_ARRAY;i++) cout << array_for_QuickSort[i] << ' ';
-    cout << endl << "Результат сортировки Вставками: ";
+    cout << endl << "Колличество обменов: " << swapCount;
+    cout << endl << "Колличество сравнений: " << ifCount;
+    ifCount = 0;
+    swapCount = 0;
+    cout << endl << "Результат сортировки Двухпутевыми вставками: ";
     TooWayInsertion(array_for_InsSort);
     for (int i=0;i<SIZE_OF_ARRAY;i++) cout << array_for_InsSort[i] << ' ';
+    cout << endl << "Колличество обменов: " << swapCount;
+    cout << endl << "Колличество сравнений: " << ifCount;
     cout << endl;
     return 0;
 }
 
 void NonRecursiveQuickSort (int array[]){
-    int i(0),j(0),L(0),R(0),s(0),x(0);
+    int i(0), j(0), L(0), R(0), s(0), x(0);
     int low[SIZE_OF_ARRAY], high[SIZE_OF_ARRAY];
 
     low[0] = 0; high[0] = SIZE_OF_ARRAY-1;
     do {
+        ifCount++;
         L = low[s]; R = high[s]; s--;
         do {
+            ifCount++;
             i = L; j = R; x = array[(L+R)/2];
             do {
-                while (array[i] < x) i++;
-                while (x < array[j]) j--;
+                ifCount++;
+                while (array[i] < x) {
+                    i++;
+                    ifCount++;
+                }
+                ifCount++;
+                while (x < array[j]) {
+                    j--;
+                    ifCount++;
+                }
+                ifCount++;
                 if (i <= j) {
                     swap(array[i], array[j]);
+                    swapCount++;
                     i++; j--;
                 }
+                ifCount++;
             } while (i<=j);
             if (i < R) {
                 s++; low[s]=i; high[s]=R;
             }
+            ifCount++;
             R=j;
         } while (L < R);
     } while (s >= 0);
@@ -64,40 +86,51 @@ void TooWayInsertion (int array[]){
     for (int i=1;i<SIZE_OF_ARRAY;i++){
         while ((k+right != k) && (array[i] > array_for_sort[k])){
             k++; left++; right--;
+            ifCount+=2;
         }
+        ifCount+=2;
         while ((k+left != k) && (array[i] < array_for_sort[k])){
             k--; left--; right++;
+            ifCount+=2;
         }
+        ifCount+=2;
         if (array[i] > array_for_sort[k]){
 
             if (left < right) {
                 for (int j=k-left-1;j<k;j++){
                     swap(array_for_sort[j],array_for_sort[j+1]);
+                    swapCount++;
                 }
                 array_for_sort[k] = array[i];
                 left++;
             } else {
                 for (int j=k+right+1;j>k+1;j--){
                     swap(array_for_sort[j],array_for_sort[j-1]);
+                    swapCount++;
                 }
                 array_for_sort[k+1] = array[i];
                 right++;
             }
+            ifCount++;
         } else {
             if (left < right) {
                 for (int j=k-left-1;j<k-1;j++){
                     swap(array_for_sort[j],array_for_sort[j+1]);
+                    swapCount++;
                 }
                 array_for_sort[k-1] = array[i];
                 left++;
             } else {
                 for (int j=k+right+1;j>k;j--){
                     swap(array_for_sort[j],array_for_sort[j-1]);
+                    swapCount++;
                 }
                 array_for_sort[k] = array[i];
                 right++;
             }
+            ifCount++;
         }
+        ifCount++;
     }
     for (int i=k-left;i<SIZE_OF_ARRAY+(k-left);i++){
         array[j] = array_for_sort[i];

a2ed109e7f8a9ee9052d18eadc5a04b96303f754
Valgulious <dmitryfedorenkof@gmail.com>
2018-04-09 20:01:39 +1000

    Create project 'Queue'

---
+++ Queue/Queue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dima on 09.04.18.
+//
+
+#include "Queue.h"
---
+++ Queue/Queue.h
@@ -0,0 +1,14 @@
+//
+// Created by dima on 09.04.18.
+//
+
+#ifndef QUEUE_QUEUE_H
+#define QUEUE_QUEUE_H
+
+
+class Queue {
+
+};
+
+
+#endif //QUEUE_QUEUE_H

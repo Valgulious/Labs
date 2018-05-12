3273ac7ee6ce315a196154d31b58937c22f58242
Valgulious <dmitryfedorenkof@gmail.com>
2018-05-12 19:45:49 +1000

    String

--- String/String/String.cpp
+++ String/String/String.cpp
@@ -30,7 +30,7 @@ String::String(const String & string)
 
 List* String::primaryInitialization(int size)
 {
-    List *pV = new List;
+    auto *pV = new List;
     pV -> symbols = new char[size];
     pV -> next = nullptr;
     pV -> prev = nullptr;
@@ -71,7 +71,7 @@ void String::writeStringInList(char const * symbols)
     }
 }
 
-String String::operator = (String string)
+String& String::operator = (String string)
 {
     List * pv = this->list;
 
@@ -89,6 +89,66 @@ String String::operator = (String string)
     }
 
     return *this;
+}
+
+const bool operator == (String & string1, String & string2)
+{
+    if (string1.length() != string2.length()) {
+        return false;
+    } else {
+        List * pv1 = string1.list;
+        List * pv2 = string2.list;
+        int i = 0, j = 0;
+
+        while (pv1) {
+            while ((i < string1.SIZE) and (j < string2.SIZE)) {
+                if (pv1 -> symbols[i] != pv2 -> symbols[j]) return false;
+                i++; j++;
+            }
+            if (i == string1.SIZE) {
+                pv1 = pv1 -> next;
+                i = 0;
+            }
+            if (j == string2.SIZE) {
+                pv2 = pv2 -> next;
+                j = 0;
+            }
+        }
+    }
+    return true;
+}
+
+String operator + (const String & string1, const String & string2)
+{
+    List * pv = string1.list;
+
+    String string0(string1.SIZE);
+
+    while (pv) {
+        string0.writeStringInList(pv->symbols);
+        pv = pv -> next;
+    }
+
+    pv = string2.list;
+    while (pv) {
+        string0.writeStringInList(pv->symbols);
+        pv = pv -> next;
+    }
+
+    return string0;
+}
+
+ostream& operator << (ostream& s, String string)
+{
+    List * pv = string.list;
+
+    while (pv) {
+        for (int i = pv ->firstSymbol; i <= pv -> lastSymbol; i++) {
+            s << pv -> symbols[i];
+        }
+        pv = pv -> next;
+    }
+    return s;
 }
 
 int String::length()

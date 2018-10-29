492d35c0af7ff0936be7dea21ceac1844be102c1
Valgulious <dmitryfedorenkof@gmail.com>
2018-10-29 18:31:02 +1000

    BTree start

--- BTreeTest/main.cpp
+++ BTreeTest/main.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
-#include "BTree.h"
+//#include "BTree.h"
+
+struct BTreeNode {
+    int *keys;
+    int t;
+    BTreeNode **children;
+    int n;
+    bool leaf;
+    BTreeNode(int _t, bool _leaf) {
+        this->t = t;
+        this->leaf = leaf;
+    }
+};
+
+struct BTree {
+    BTreeNode *root;
+    int degree;
+    BTree(int _degree) {
+        this->degree = _degree;
+    }
+};
+
+void traverse();
+BTreeNode *search(int _key);
+int findKey(int _key);
+void insertNonFull(int _key);
+void splitChild(int idx, BTreeNode *child);
+void remove(int _key);
+void removeFromLeaf(int _idx);
+void removeFomNoLeaf(int _idx);
+int getPred(int _idx);
+int getSucc(int _idx);
+void fill(int _idx);
+void borrowFromPrev(int _idx);
+void borrowFromNext(int _idx);
+void merge(int _idx);
+
+void insert(int _key);
 
 int main() {
-    BTree t(3);
-
-    t.insert(1);
-    t.insert(3);
-    t.insert(7);
-    t.insert(10);
-    t.insert(11);
-    t.insert(4);
-    t.remove(10);
-    t.traverse();
 
     return 0;
 }
